ddf6b55783f46903446e0cef3d88a3a82b2d6e02
mzakya18 <65690190+mzakya18@users.noreply.github.com>
2026-08-02 01:25:13 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,43 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+const CACHE_NAME = 'bicara-pecs-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png' // Pastikan nama ini sama dengan nama file logo di GitHub Anda
+];
+
+// Install Service Worker dan simpan file ke memori HP (Cache)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
+// Hapus memori (Cache) lama jika ada update versi baru
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Ambil data dari memori (Offline) saat tidak ada internet
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Kembalikan file dari cache jika ada, jika tidak ambil dari internet
+        return response || fetch(event.request);
+      })
+  );
 });
