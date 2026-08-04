1c5b8fd544d8f7f18f589a98578029a990d99aa3
Muhammad Zaky Akbar <65690190+mzakya18@users.noreply.github.com>
2026-08-04 21:39:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,27 @@
-const CACHE_NAME = 'bicara-pecs-v4';
+const CACHE_NAME = 'bicara-pecs-v2';
 const urlsToCache = [
   './',
   './index.html',
+  './style.css',
+  './script.js',
   './manifest.json',
-  './icon-512.png' // Pastikan nama ini sama dengan nama file logo di GitHub Anda
+  './modules/speech.js',
+  './modules/storage.js',
+  './modules/render.js',
+  './database/kata-kerja.js'
+  // Tambahkan path file lain
 ];
 
-// Install Service Worker dan simpan file ke memori HP (Cache)
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Hapus memori (Cache) lama jika ada update versi baru
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Ambil data dari memori (Offline) saat tidak ada internet
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Kembalikan file dari cache jika ada, jika tidak ambil dari internet
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
